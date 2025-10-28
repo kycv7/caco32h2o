26e67a86ab7507c3a0e9aeb20654da388a9f8975
kycv7 <kycv7@outlook.com>
2025-10-28 14:02:26 -0600

    interpolación y parámetros

--- lab.py
+++ lab.py
@@ -12,3 +12,11 @@ ka_file=input("ingresa la ruta del csv de ka: ")
 df_kps=pd.read_csv(kps_file)
 df_kf=pd.read_csv(kf_file)
 df_ka=pd.read_csv(ka_file)
+#interpolación
+kf_func=interp1d(df_kf["Ligando_M"],df_kf["Kf"],fill_value="extrapolate")
+ka_func=interp1d(df_ka["H_total_M"],df_ka["Ka"],fill_value="extrapolate")
+kps_func=interp1d(df_kps["Ca2+_M"],df_kps["Kps"],fill_value="extrapolate")
+#parámetros y rango
+CaCO3_list=np.linspace(0.005,0.05,30)
+H2SO4_list=np.linspace(0.005,0.05,30)
+
