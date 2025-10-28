53193c526d44ea3e0c7dbe9085769d120bc5d427
kycv7 <kycv7@outlook.com>
2025-10-28 11:38:07 -0600

    importar librerias

---
+++ lab.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
