9b294dbb918ab59d20ade470b511fd0305dcbc16
kycv7 <kycv7@outlook.com>
2025-10-28 13:34:12 -0600

    leer archivos csv

--- lab.py
+++ lab.py
@@ -5,3 +5,10 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
+#leer CSV
+kps_file=input("ingresa la ruta del CSV de kps: ")
+kf_file=input("ingresa la ruta del csv de kf: ")
+ka_file=input("ingresa la ruta del csv de ka: ")
+df_kps=pd.read_csv(kps_file)
+df_kf=pd.read_csv(kf_file)
+df_ka=pd.read_csv(ka_file)
